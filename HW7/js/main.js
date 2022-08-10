3bf5ca5d7a5b97ea14c36f64c3ad1c645088fb68
voichuk1995 <S1e9r9u5ypro100easy>
2022-08-10 10:07:21 +0300

    HW7+JS

---
+++ HW7/js/main.js
@@ -0,0 +1,6 @@
+let inputCheck = document.querySelector('.content__row_input:checked')
+let itemInput = document.querySelector('.content__row')
+console.log('djdjdjdj')
+if(inputCheck) {
+    itemInput.classList('.content__row--active')
+}
